6c479eafcc5bfa1d419f652f94bed8cb306caba2
Amit Singh <c.a.singh@hotmail.com>
2023-02-21 23:21:18 -0500

    Clean up old data

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,10 +9,6 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
   heroes: Hero[] = [];
 
